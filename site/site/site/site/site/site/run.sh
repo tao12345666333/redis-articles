480733d31be1052c72adc4ff9bb8c65aa6d1ab3e
TaoBeier <zhangjintao9020@gmail.com>
2016-11-15 01:01:26 +0800

    Deployed 1e01d08 with MkDocs version: 0.15.3

---
+++ site/site/site/site/site/site/run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+usage="Usage: $0 [update|deploy] title url"
+
+case $1 in
+    (update)
+        git checkout master
+        git pull origin master
+        echo "*"  `date -d now +%Y-%m-%d`"  "[$2]\($3\) >> README.md
+        echo "" >> README.md
+        ;;
+
+    (deploy)
+        echo "deploy"
+        # for mkdocs limit
+        mv README.md index.md
+        mkdocs gh-deploy
+        echo "publish to github"
+        mv index.md README.md
+        echo "finish"
+
+    (*)
+        echo $usage
+        exit 1
+        ;;
+
+esac
